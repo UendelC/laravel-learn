8ff51f62135677a7191c9d54245bcc6f4fe19aea
Uendel Couto <uendel.couto@gmail.com>
2020-03-20 11:37:39 -0300

    Initial Commit

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,3 @@
+<h1>Minha página de Contato</h1>
+<a href="mailto:meu.email@gmail.com">Mandar um e-mail.</a>
+{{$titulo}}
---
+++ resources/views/produto/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layout.app')
+@section('title', $produto->titulo)
+@section('content')
+    <h1>{{ $produto->titulo }}</h1>
+    <div class="row">
+        <div class="col-md-6 col-md-3">
+            <ul>
+                <li>Referência: {{ $produto->referencia }}</li>
+                <li>Preço: {{$produto->preco}}</li>
+            <li>Adicionado em: {{$produto->created_at}}</li>
+    </ul>
+        </div>
+    </div>
+    <p>{{$produto->descricao}}</p>
+
+    @if(file_exists('./img/produtos/'.md5($produto->id).'.jpg'))
+        <div class="col-md-6 col-md-3">
+            {{Html::image(asset('img/produtos/'.md5($produto->id).'.jpg'))}}
+        </div>
+    @endif
+    <a href="javascript:history.go(-1)">Voltar</a>
+@endsection
