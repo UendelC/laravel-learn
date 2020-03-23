a480c45ad407c6906b48cfdfec01b16e14723d25
Uendel Couto <uendel.couto@gmail.com>
2020-03-23 11:34:23 -0300

    Limpando código

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,8 +25,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/produtos') }}">
+                   Garrafas online
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -35,7 +35,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li><a href="{{ url('/produtos/')}}">Produtos</a></li>
+                        <li><a href="{{ url('/contato/') }}">Contato</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
